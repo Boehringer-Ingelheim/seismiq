9dab381382f4d89460d212f67c3ed873686ef3ac
e-dorigatti <emilio.dorigatti@gmail.com>
2025-07-28 16:23:07 +0200

    updated fraggenie path

--- scripts/fraggenie.sh
+++ scripts/fraggenie.sh
@@ -7,7 +7,7 @@ OUTFILE=$3
 FRAG_RECURSION_DEPTH=$4
 MIN_FRAG_MASS=$5
 
-FG_FOLDER=./development/FragGenie
+FG_FOLDER=./dev/FragGenie
 java -Xmx32000m -cp $FG_FOLDER/target/liv-metfrag-0.1.0-SNAPSHOT.jar:$FG_FOLDER/target/lib/* \
     uk.ac.liverpool.metfrag.MetFragFragmenter \
     $BASEDIR/$INFILE $BASEDIR/$OUTFILE smiles $FRAG_RECURSION_DEPTH $MIN_FRAG_MASS 2048 100000 false METFRAG_MZ METFRAG_FORMULAE
